9ffcf5580934c0347e16ef0407deb9f809dfee93
Ludwig SILVAIN <ludwig@silvain.eu>
2022-04-12 21:23:25 +0200

    fix

--- ML_torch.py
+++ ML_torch.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import numpy as np
 import pandas
 import torch
@@ -15,22 +17,29 @@ class NeuralNetworkClassificationModel(nn.Module):
     def __init__(self, input_dim, output_dim=1):
         super(NeuralNetworkClassificationModel, self).__init__()
         self.input_layer = nn.Linear(input_dim, 310)
-        self.hidden_layers = [
-            nn.Linear(310, 310),
-            nn.Linear(310, 310),
-            nn.Linear(310, 310),
-            nn.Linear(310, 155),
-            nn.Linear(155, 155),
-            nn.Linear(155, 77),
-        ]
+
+        self.hidden_layer1 = nn.Linear(310, 310)
+        self.hidden_layer2 = nn.Linear(310, 310)
+        self.hidden_layer3 = nn.Linear(310, 310)
+        self.hidden_layer4 = nn.Linear(310, 155)
+        self.hidden_layer5 = nn.Linear(155, 155)
+        self.hidden_layer6 = nn.Linear(155, 77)
 
         self.output_layer = nn.Linear(77, output_dim)
         self.relu = nn.ReLU()
 
     def forward(self, x):
         out = self.relu(self.input_layer(x))
-        for c in self.hidden_layers:
-            out = self.relu(c(out))
+        # hiden : List[nn.Module] = self.hidden_layers;
+        # for c in hiden:z
+        #     out = self.relu(c(out))
+        # out = self.relu(self.hidden_layers(out))
+        out = self.relu(self.hidden_layer1(out))
+        out = self.relu(self.hidden_layer2(out))
+        out = self.relu(self.hidden_layer3(out))
+        out = self.relu(self.hidden_layer4(out))
+        out = self.relu(self.hidden_layer5(out))
+        out = self.relu(self.hidden_layer6(out))
         out = self.output_layer(out)
         return out
 
@@ -68,8 +77,11 @@ with torch.no_grad():
 train_acc = lib.get_accuracy_multiclass(predictions_train, y_train)
 test_acc = lib.get_accuracy_multiclass(predictions_test, y_test)
 
-print(f"Training Accuracy: {round(train_acc*100,3)}")
-print(f"Test Accuracy: {round(test_acc*100,3)}")
+print(f"Training Accuracy: {round(train_acc * 100, 3)}")
+print(f"Test Accuracy: {round(test_acc * 100, 3)}")
 
 torch.save(model.state_dict(), "torch.pt")
 torch.save(model, "torch_all.pt")
+
+model_scripted = torch.jit.script(model)
+model_scripted.save("traced_resnet_model.pt")
