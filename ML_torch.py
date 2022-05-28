a5ca49f275fb1aac0975e346bcb7b1f4c922037a
Simon <simon.heban@ferenost.fr>
2022-05-28 16:57:42 +0200

    fix: rendu final

--- ML_torch.py
+++ ML_torch.py
@@ -1,67 +1,62 @@
-from typing import List
-
+import matplotlib.pyplot as plt
 import numpy as np
-import pandas
 import torch
+import torch.nn as nn
 
 import lib
-import torch.nn as nn
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
-import seaborn as sn
 
 
 class NeuralNetworkClassificationModel(nn.Module):
     def __init__(self, input_dim, output_dim=1):
         super(NeuralNetworkClassificationModel, self).__init__()
-        self.input_layer = nn.Linear(input_dim, 310)
-
-        self.hidden_layer1 = nn.Linear(310, 310)
-        self.hidden_layer2 = nn.Linear(310, 310)
-        self.hidden_layer3 = nn.Linear(310, 310)
-        self.hidden_layer4 = nn.Linear(310, 155)
-        self.hidden_layer5 = nn.Linear(155, 155)
-        self.hidden_layer6 = nn.Linear(155, 77)
-
-        self.output_layer = nn.Linear(77, output_dim)
+        max_layer = 200
+        self.input_layer = nn.Linear(input_dim, max_layer)
+        self.hidden_layer1 = nn.Linear(max_layer, max_layer)
+        self.output_layer = nn.Linear(max_layer, output_dim)
         self.relu = nn.ReLU()
+
+        self.input_layer = self.input_layer.cuda()
+        self.hidden_layer1 = self.hidden_layer1.cuda()
+        self.output_layer = self.output_layer.cuda()
+        self.relu = self.relu.cuda()
 
     def forward(self, x):
         out = self.relu(self.input_layer(x))
-        # hiden : List[nn.Module] = self.hidden_layers;
-        # for c in hiden:z
-        #     out = self.relu(c(out))
-        # out = self.relu(self.hidden_layers(out))
         out = self.relu(self.hidden_layer1(out))
-        out = self.relu(self.hidden_layer2(out))
-        out = self.relu(self.hidden_layer3(out))
-        out = self.relu(self.hidden_layer4(out))
-        out = self.relu(self.hidden_layer5(out))
-        out = self.relu(self.hidden_layer6(out))
         out = self.output_layer(out)
         return out
 
 
-dataframe = lib.load_data('data/result.csv')
+dataframe = lib.load_data('data/result_stockfish.csv')
+print(dataframe.columns)
 lib.correlation_matrix(dataframe)
 (x_train, x_test, y_train, y_test), nbColumns = lib.extract_xy(dataframe)
 x_train, x_test, y_train, y_test = lib.normalize(x_train, x_test, y_train, y_test)
 
-model = NeuralNetworkClassificationModel(nbColumns, 17)
+x_train = x_train.cuda()
+y_train = y_train.cuda()
+x_test = x_test.cuda()
+y_test = y_test.cuda()
+
+model = NeuralNetworkClassificationModel(nbColumns, 20)
+model.parameters()
+model.cuda()
 
 learning_rate = 0.01
 criterion = nn.CrossEntropyLoss()
+criterion.cuda()
 
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-num_epochs = 2000
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0.001)
+# optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+num_epochs = 1000
 train_losses = np.zeros(num_epochs)
 test_losses = np.zeros(num_epochs)
 
-train_losses, test_losses, test_mse, train_mse = lib.train_network(model, optimizer, criterion, x_train, y_train, x_test, y_test, num_epochs,
-                                              train_losses, test_losses)
+train_losses, test_losses, test_mse, train_mse = lib.train_network(model, optimizer, criterion, x_train, y_train,
+                                                                   x_test, y_test, num_epochs,
+                                                                   train_losses, test_losses)
 
 plt.figure(figsize=(10, 10))
 plt.plot(train_losses, label='train loss')
@@ -69,17 +64,19 @@ plt.plot(test_losses, label='test loss')
 plt.legend()
 plt.show()
 
-plt.figure(figsize=(10, 10))
-plt.plot(train_mse, label='train mse')
-plt.plot(test_mse, label='test mse')
-plt.legend()
-plt.show()
-
 predictions_train = []
 predictions_test = []
 with torch.no_grad():
     predictions_train = model(x_train)
+    predictions_train = predictions_train.cpu()
     predictions_test = model(x_test)
+    predictions_test = predictions_test.cpu()
+
+
+x_train = x_train.cpu()
+x_test = x_test.cpu()
+y_train = y_train.cpu()
+y_test = y_test.cpu()
 
 train_acc = lib.get_accuracy_multiclass(predictions_train, y_train)
 test_acc = lib.get_accuracy_multiclass(predictions_test, y_test)
--- lib.py
+++ lib.py
@@ -1,48 +1,81 @@
-import math
-
 import numpy as np
 import pandas
-import tqdm
+import seaborn as sn
 import torch
-import torch.nn as nn
+import tqdm
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-import seaborn as sn
 
 
-def load_data(file: str = 'data/result.csv') -> pandas.DataFrame:
-    dataframe = pandas.read_csv('data/result.csv', sep=";")
-
+def load_data(file: str = 'data/result_stockfish.csv') -> pandas.DataFrame:
+    dataframe = pandas.read_csv(file, sep=";")
+    # dataframe = dataframe.sample(frac=1).reset_index(drop=True)
     dataframe = dataframe[dataframe["current_player"] == "white"]
-
+    dataframe = dataframe[dataframe["current_step"] >= 100]
+    dataframe["level_diff"] = dataframe["level_white"]
     del dataframe["id"]
     del dataframe["level_white"]
     del dataframe["current_player"]
-    del dataframe["white_pieces_k"]
-    del dataframe["white_pieces_lost_k"]
-    del dataframe["black_pieces_k"]
-    del dataframe["black_pieces_lost_k"]
+    del dataframe["level_back"]
+    del dataframe["current_step"]
+    # del dataframe["cp_diff"]
+    # del dataframe["cp"]
+
+    # del dataframe["white_pieces_k"]
+    # del dataframe["black_pieces_k"]
+
+    pieces = ["b", "n", "p", "q", "r", "k"]
+    for i in pieces:
+        del dataframe["white_pieces_lost_" + i]
+        del dataframe["white_pieces_" + i]
+        del dataframe["black_pieces_lost_" + i]
+        del dataframe["black_pieces_" + i]
+
+    # dataframe["white_pieces"] = 0
+    # dataframe["black_pieces"] = 0
+    # # dataframe["black_pieces_lost"] = 0
+    # # dataframe["white_pieces_lost"] = 0
+    # for i in pieces:
+    #     dataframe["white_pieces"] += dataframe["white_pieces_" + i]
+    #     dataframe["black_pieces"] += dataframe["black_pieces_" + i]
+    #     # dataframe["white_pieces_lost"] += dataframe["white_pieces_lost_" + i]
+    #     # dataframe["black_pieces_lost"] += dataframe["black_pieces_lost_" + i]
+    #
+    # new_dataframe = pandas.DataFrame()
+    # dataframe = new_dataframe.assign(level_black=dataframe["level_back"].values,
+    #                                  cp_diff=dataframe["cp_diff"].values,
+    #                                  cp=dataframe["cp"].values,
+    #                                  white_pieces=dataframe["white_pieces"].values,
+    #                                  black_pieces=dataframe["black_pieces"].values,
+    #                                  # white_pieces_lost=dataframe["white_pieces_lost"].values,
+    #                                  # black_pieces_lost=dataframe["black_pieces_lost"].values,
+    #                                  current_step=dataframe["current_step"].values,
+    #                                  level_diff=dataframe["level_diff"].values)
     return dataframe
 
 
 def extract_xy(dataframe: pandas.DataFrame):
-    dataframe.sample(frac=1)
+    # dataframe.sample(frac=1)
     x = dataframe
+    # x["current_player"] = x["current_player"].map(lambda z : 0 if z == "white" else 1)
+
     y = dataframe["level_diff"]
 
     del x["level_diff"]
 
-    y = y + 8
+    y = y - 1
+    # y.loc[y < 0] = 2
+    # y.loc[y > 0] = 1
 
-    return train_test_split(x.values, y.values, test_size=0.8), len(x.columns)
+    return train_test_split(x.values, y.values, test_size=0.5), len(x.columns)
 
 
 def normalize(x_train, x_test, y_train, y_test):
     scaler = StandardScaler()
 
-    x_train = scaler.fit_transform(x_train)
-    x_test = scaler.transform(x_test)
+    # x_train = scaler.fit_transform(x_train)
+    # x_test = scaler.transform(x_test)
     # y_test = y_test + 8
     # y_train = y_train + 8
 
@@ -70,41 +103,38 @@ def correlation_matrix(dataframe: pandas.DataFrame):
 def train_network(model, optimizer, criterion, X_train, y_train, X_test, y_test, num_epochs, train_losses, test_losses):
     train_mse = np.zeros(num_epochs)
     test_mse = np.zeros(num_epochs)
-    mse = nn.MSELoss()
-
+    # mse = nn.MSELoss()
     for epoch in tqdm.trange(num_epochs):
-        # clear out the gradients from the last step loss.backward()
         optimizer.zero_grad()
 
         # forward feed
         y_pred = model(X_train)
-
+        y_pred.cuda()
         # calculate the loss
         loss_train = criterion(y_pred, y_train)
-
+        # clear out the gradients from the last step loss.backward()
         # backward propagation: calculate gradients
         loss_train.backward()
-
         # update the weights
         optimizer.step()
+        with torch.no_grad():
+            model.eval()
+            output_test = model(X_test)
+            loss_test = criterion(output_test, y_test)
 
-        output_test = model(X_test)
-        loss_test = criterion(output_test, y_test)
+            train_losses[epoch] = loss_train.item()
+            test_losses[epoch] = loss_test.item()
 
-        train_losses[epoch] = loss_train.item()
-        test_losses[epoch] = loss_test.item()
-
-        mse_test = mse(torch.max(y_pred, 1).values, y_train)
-
-        train_mse[epoch] = mse_test.item()
-        test_mse[epoch] = mse(torch.max(output_test, 1).values, y_test).item()
+        # mse_test = mse(torch.max(y_pred, 1).values, y_train)
+        #
+        # train_mse[epoch] = mse_test.item()
+        # test_mse[epoch] = mse(torch.max(output_test, 1).values, y_test).item()
 
         if (epoch + 1) % 25 == 0:
             print(
-                f"Epoch {epoch + 1}/{num_epochs}, Train Loss: {loss_train.item():.4f}, Test Loss: {loss_test.item():.4f}, MSE: {mse_test.item():.4f}")
+                f"Epoch {epoch + 1}/{num_epochs}, Train Loss: {loss_train.item():.4f}, Test Loss: {loss_test.item():.4f}")
 
     return train_losses, test_losses, test_mse, train_mse
-
 
 def get_accuracy_multiclass(pred_arr, original_arr):
     if len(pred_arr) != len(original_arr):
